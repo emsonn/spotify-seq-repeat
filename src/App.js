e83a3e57a8703be583dc14539729de02bbb15525
emsonn <eabrahamson@knights.ucf.edu>
2019-08-23 03:50:07 -0400

    Changed template wording

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, React</h1>
+        <h2>Play a song on your Spotify account to get started.</h2>
       </div>
     );
   }
